e10071dfeb391128d5c187da8bf8c8ab1f5a8cb7
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-01 11:52:20 +0530

    First question  of this journey

--- DAY1/problem1.py
+++ DAY1/problem1.py
@@ -1,4 +1,19 @@
 list1 = [1,2,2,3,3,3]
+dict1 = {}
+count = 0
 for i in list1:
-    if list1.count(i):
-        print({i,'->',list1})
+    if i in dict1:
+       dict1[i] = dict1[i] + 1
+    else:
+        dict1[i] = 1
+for key, value in dict1.items():
+  print(key, "->", value)
+
+       
+
+        
+
+    
+    
+        
+
