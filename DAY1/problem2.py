8fdd8cde0cb82b5ce9e27a8598e6086efe4ee72f
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-02 12:11:35 +0530

    2D array

--- DAY1/problem2.py
+++ DAY1/problem2.py
@@ -1,3 +1,6 @@
 import numpy as np
 arr1 = np.array([1,2,3,4,5])
 print("1D array using numpy in python:\n",arr1)
+print('')
+arr2 = np.array([[2,4,6],[3,5,7]])
+print("2D array using numpy in python:\n",arr2)
