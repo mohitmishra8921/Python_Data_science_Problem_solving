94b2aff7cfa1cef7e75e6559000ed35a00ff4d1e
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-03 21:45:27 +0530

    check properties of 1d 2d array

--- DAY1/problem2.py
+++ DAY1/problem2.py
@@ -4,3 +4,11 @@ print("1D array using numpy in python:\n",arr1)
 print('')
 arr2 = np.array([[2,4,6],[3,5,7]])
 print("2D array using numpy in python:\n",arr2)
+# now we use propertues of numpy to check the properties of numpy areays created by us 
+a = arr1.shape
+print(a)
+
+b = arr2.size
+print(b)
+
+
