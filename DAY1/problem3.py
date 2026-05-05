b565dbf136a913a96094124095aa0ec13d97b6cb
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-05 10:56:43 +0530

    using pandas library of python

---
+++ DAY1/problem3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = {"Name":["Aman","Riya","Karan","Sneha"],
+      "Maths":[85,78,92,88],
+      "Science":[90,82,87,95],
+      "English":[88,80,91,89]
+      } 
+a = pd.DataFrame(df)
+print(a)
+         
