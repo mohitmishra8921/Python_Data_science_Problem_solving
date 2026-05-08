83505baba5298c8bbff8ded9014dd0ad6b1640b8
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-08 14:08:24 +0530

    Accessing elements of numpy array

---
+++ DAY2/problem2.py
@@ -0,0 +1,8 @@
+import numpy as  np
+arr = np.array([10,20,30,40,50,60])
+print(arr)
+a = arr[1]# This is the most simplest way of indexing or accessing single elements from the the array 
+print(a)
+# we can also access elements from the array in the form of slicing to access the array 
+a1 = arr[0:5]
+print(a1)
