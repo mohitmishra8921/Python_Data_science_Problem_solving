bfe5ea1907698e962577e525f83e46495edf75de
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-09 12:16:11 +0530

    Today i have taken real data from online makrs of students and first store them in the form of numpy array than frame that marks in form of table using dataframes of pandas

---
+++ DAY2/problem3.py
@@ -0,0 +1,13 @@
+# Create a marks dataset for 5 students and perform analysis.
+
+import numpy as np 
+import pandas as pd
+marks = np.array([[78,85,80],
+                 [90,88,92],
+                 [67,70,72],
+                 [95,98,96],
+                 [82,79,85]])
+columns = ["Maths","Science","English"]
+print(marks.shape)
+df = pd.DataFrame(marks,columns=columns,index=["A","B","C","D","E"])
+print(df)
