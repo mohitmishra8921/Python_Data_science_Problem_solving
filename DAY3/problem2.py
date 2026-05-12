4f4c1f9e42588a068b18871f70d6e2b07cbf5625
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-12 21:57:50 +0530

    reshap ing array

---
+++ DAY3/problem2.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr1 = np.array([[10,20,30],
+               [40,50,60],
+               [10,20,30],
+               [40,50,60]])
+#here we use simple proprties to change its shape as well as make 2d or 3d array into 1d array
+a = arr1.reshape(2,6) # we can change shapes but need to to reminf one thing in my mind is no of elements must be same while giving new shape here 2*6 = 12 elements
+print(a)
