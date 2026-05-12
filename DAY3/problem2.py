2b42a42d47de48d969547be5f3e065d9aa672d9e
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-12 22:00:45 +0530

    flattening the arry

--- DAY3/problem2.py
+++ DAY3/problem2.py
@@ -6,3 +6,5 @@ arr1 = np.array([[10,20,30],
 #here we use simple proprties to change its shape as well as make 2d or 3d array into 1d array
 a = arr1.reshape(2,6) # we can change shapes but need to to reminf one thing in my mind is no of elements must be same while giving new shape here 2*6 = 12 elements
 print(a)
+b = arr1.flatten()
+print(b) #it is normally used for making 1d array
