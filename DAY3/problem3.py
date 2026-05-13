ac011690c0a3413c21b34dbfd605a8e846418c3e
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-13 21:48:13 +0530

    vectorisation on arrays

--- DAY3/problem3.py
+++ DAY3/problem3.py
@@ -1,5 +1,9 @@
 import numpy as np
 arr = np.array([1,2,3,4])#today i will apply vectorisation on array without using loops 
-b= arr+5 #here we add 5 to each elements without running a loop 
+c= arr+5 #here we add 5 to each elements without running a loop 
 print(b)
 
+a = [1,2,3]
+b = [4,5,6]
+d = a*b
+print(d)
