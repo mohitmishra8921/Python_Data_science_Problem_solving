cb1b3ffabe2780885ad795cd82092359918a4e8b
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-14 23:05:04 +0530

    min max temp of week by usin min max function  of the week

--- DAY3/problem4.py
+++ DAY3/problem4.py
@@ -2,3 +2,7 @@ import numpy as np
 array =np.array([32, 34, 31, 29, 35, 36, 33])
 a = array.mean()#Use to find the average temperature of given following temp of weaks
 print("The average temp of the week is :",a)
+b = array.max()#to calculate the maximum temp on any day of the week 
+print("Maximum temp of week",b)
+c = array.min()#minimum temp of the week 
+print("Minimum temp of week",c)
