f8c00d49ddf81361b1fa90558426a45b3fc4b1e9
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-14 22:45:22 +0530

    finding average of temp by storing temp of normal days of week

---
+++ DAY3/problem4.py
@@ -0,0 +1,4 @@
+import numpy as np 
+array =np.array([32, 34, 31, 29, 35, 36, 33])
+a = array.mean()#Use to find the average temperature of given following temp of weaks
+print("The average temp of the week is :",a)
