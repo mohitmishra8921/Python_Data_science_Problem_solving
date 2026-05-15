f29a8b440faec987b9852be4b21cd18e94a960e9
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-15 22:10:09 +0530

    vectorised the problem

--- DAY3/problem4.py
+++ DAY3/problem4.py
@@ -6,3 +6,7 @@ b = array.max()#to calculate the maximum temp on any day of the week
 print("Maximum temp of week",b)
 c = array.min()#minimum temp of the week 
 print("Minimum temp of week",c)
+
+#my todays work on this problem of temperature of the whole week is to vectorized it by add two degrees in each temp 
+d = array + 2 # here in numpy we simply add 2 to each day temp without creatong another aaray without using loops due to which it becomes memory costly
+print(d)
