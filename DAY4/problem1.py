4698346825c3ff3f7a061c6b3b0964eebd0d233a
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-16 22:12:46 +0530

    find vowels in string

--- DAY4/problem1.py
+++ DAY4/problem1.py
@@ -1,6 +1,21 @@
-list1 = [1,2,3,4,5,6,7,8]
-even_num = []
-for i in list1:
-    if i %2==0:
-        even_num.append(i)
-print("The even numbers from the list are :",even_num)
+# list1 = [1,2,3,4,5,6,7,8]
+# even_num = []
+# for i in list1:
+#     if i %2==0:
+#         even_num.append(i)
+# print("The even numbers from the list are :",even_num)
+
+string1 = "data science"
+
+vowels_count = {}
+
+for ch in string1:
+    if ch in "aeiou":
+
+        if ch in vowels_count:
+            vowels_count[ch] += 1
+
+        else:
+            vowels_count[ch] = 1
+
+print(vowels_count)
