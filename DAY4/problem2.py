4ae7258dcea85cabbcdf076f0b6b9273cf4bfc19
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-17 14:51:36 +0530

    Booleann masking with the help of amnd or operatons between two conditions seperately

--- DAY4/problem2.py
+++ DAY4/problem2.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-# arr = np.array([2,5,8,1,9,3,10])
-# mask1 = arr>5
-# mask2 = arr<5
-#print(arr[mask1])#This is for greater than 5
-#print(arr[mask2])#I can also print less than 5 by reversing comparison operator from greater than to less than in the same method
+arr = np.array([2,5,8,1,9,3,10])
+mask1 = arr>5
+mask2 = arr<5
+print(arr[mask1])#This is for greater than 5
+print(arr[mask2])#I can also print less than 5 by reversing comparison operator from greater than to less than in the same method
 
 #It is one of the famous method masking as well as there is also one of the method like this but that use for indexing multiple elements at one time by this feature of numpy 
 
