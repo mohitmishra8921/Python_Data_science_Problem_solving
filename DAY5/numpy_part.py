209fedd1af9a32aa80fdef7e85ee61856759355f
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-24 17:49:55 +0530

    numpy practice part of day 5th

---
+++ DAY5/numpy_part.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array([1,2,2,3,4,4,5])# It looks very easy for day 5 numpy part but when i strat doing this question i have learned two things one never name your file as module name before i saved my folder as numpy.py so it arises an attribute error
+a = np.sort(arr)# Arrray comes in an ascending order
+b = np.std(arr)#Standard deviation of above array mean meadian i had already done in previous practice questions 
+c = np.unique(arr)#Get a list of elements whiich avoid duplicates
+
+print(a)
+print(b)
+print(c)
