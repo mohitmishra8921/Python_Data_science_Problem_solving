2abdf36084ac2a1bba5077eab483af2c954b99e7
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-27 22:12:18 +0530

    Data filtering in  using numpy built in function

--- DAY5/problem2.py
+++ DAY5/problem2.py
@@ -16,3 +16,31 @@ print(set(arr[b]))
 #3rd task
 mask1= arr>a # To find the numbers in array which is greater than the mean of the above elements of array 
 print(arr[mask1])
+
+
+#4th task 
+#to replace all even numbvers with 0
+# arr[arr%2==0]=0
+# print(arr.copy())
+
+#Mini project task
+#marks of student mini project for data analyst
+marks = np.array([78,45,90,66,84,39,72,91])
+a = marks.max()#1toppr marks
+print(f"Marks of toppr:\n{a}")
+b = marks.argmax()#2 index of toprs marks
+print(f"Index of topprs mask:\n{b}")
+
+c = arr<40
+print(f"marks of failed students:\n{arr[c]}")
+
+d = arr>75
+print(f"Marks of distinction student:\n{arr[d]}")
+
+passed = marks>=40
+print(f"No of students passed in exams :\n{passed}")
+
+# In above step we got true for passe students in the terminal now by using one more function of numpy we count how many students pased the exam
+
+print(f"No of studnets passed the exams:\n{passed.sum()}")
+
