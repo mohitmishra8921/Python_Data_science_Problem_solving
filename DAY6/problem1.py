36b270ee1fe10e0eeec002329d4d5496072aea90
Mohit Mishra <mohitmishra8921@gmail.com>
2026-05-29 21:21:01 +0530

    frequency of words in sentence

--- DAY6/problem1.py
+++ DAY6/problem1.py
@@ -3,3 +3,18 @@ num = [1,2,3,5,6]
 for i in num:
     if i + 1 not in num:
         print(f"Missing numbers in the list:\n{i + 1}")
+
+#Problem 2 
+sentence = "data science is fun and data is powerful".split()
+
+freq = {}
+
+for word in sentence:
+
+    if word in freq:
+        freq[word] += 1
+
+    else:
+        freq[word] = 1
+
+print(freq)
