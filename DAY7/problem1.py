0fa5a9644a1444f476093aeb66e48d41a18c93f0
Mohit Mishra <mohitmishra8921@gmail.com>
2026-06-04 23:51:34 +0530

    question 2 of day 7

--- DAY7/problem1.py
+++ DAY7/problem1.py
@@ -1,6 +1,16 @@
-#DAY 7 first problem solving question 
+#DAY 7 first problem solving question
+# Question 1 
 string1 = "programming"
 for ch in string1:
     if string1.count(ch)==1:
         print(f"First non repeating character is :\n",ch)
         break
+
+#Question 2
+list1 = [10, 5, 20, 8, 15]
+largest = 0 
+smallest = 0
+for i in list1:
+    if i > largest:
+        largest = i
+print(f"Largest element in the unsorted list:\n{largest}")
