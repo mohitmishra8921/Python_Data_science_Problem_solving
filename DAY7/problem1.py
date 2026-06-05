9bd44348d6db5912a68907393b5784bd2e3a8807
Mohit Mishra <mohitmishra8921@gmail.com>
2026-06-05 21:28:32 +0530

    problem solving

--- DAY7/problem1.py
+++ DAY7/problem1.py
@@ -7,10 +7,29 @@ for ch in string1:
         break
 
 #Question 2
+# list1 = [10, 5, 20, 8, 15]
+# largest = 0 
+# for i in list1:
+#     if i > largest:
+#         largest = i
+# print(f"Largest element in the unsorted list:\n{largest}")
+#Question 3 
+#Is it same before just one chng that we have to find secodn lrgest in th elist without using sorting 
+
+
 list1 = [10, 5, 20, 8, 15]
-largest = 0 
-smallest = 0
+
+largest = 0
+second_largest = 0
+
 for i in list1:
+
     if i > largest:
+        second_largest = largest
         largest = i
-print(f"Largest element in the unsorted list:\n{largest}")
+
+    elif i > second_largest:
+        second_largest = i
+
+print("Largest:", largest)
+print("Second Largest:", second_largest)
