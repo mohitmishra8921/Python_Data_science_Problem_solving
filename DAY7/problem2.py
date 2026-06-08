0579e6a03c1d65600f65e5802382f8e3ab5c9820
Mohit Mishra <mohitmishra8921@gmail.com>
2026-06-08 23:21:42 +0530

    Another problemn for day 7

---
+++ DAY7/problem2.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([78, 45, 90, 66, 84, 39, 72, 91, 58, 88])
+a = np.sort(arr)
+print(a)
+
+b =a[-3:]
+print(f"First three highest marks:\n{b}")
+c = []
+for i in b[::-1]:
+        c.append(i)
+print("First three highest marks:\n",c)
