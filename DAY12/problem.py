35200870ac8f09e3961e3c6f5e9c74ff1af00113
Mohit Mishra <mohitmishra8921@gmail.com>
2026-06-22 07:50:16 +0530

    another question for day 2

--- DAY12/problem.py
+++ DAY12/problem.py
@@ -4,3 +4,15 @@ for i in nums :
     num.append(i*2)
 print(num)
 
+words = ["apple", "banana", "apple", "orange", "banana", "apple"]
+
+dicti = {}
+for ch in words :
+    
+    if ch in dicti:
+        dicti[ch]+=1
+    else:
+        dicti[ch] = 1
+for key,values in dicti.items():
+    print("Frequency of items in the list:\n",key,values)
+
