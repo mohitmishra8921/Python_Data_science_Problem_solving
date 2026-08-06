130eb12014e5d3a3a6300f8302936243fa9ae918
Mohit Mishra <mohitmishra8921@gmail.com>
2026-08-06 13:46:13 +0530

    After a huge break starting journey by solving file handling question

--- DAY13/problem.py
+++ DAY13/problem.py
@@ -1,18 +1,38 @@
 
 #First task of day 13 count words in a string
-sentence = "data science is fun and data science is powerful".split()
-count = 0
+# sentence = "data science is fun and data science is powerful".split()
+# count = 0
 
-for ch in sentence:
-    count+=1
-print(count)
+# for ch in sentence:
+#     count+=1
+# print(count)
 
 
-#Second task of day 13 to find the unique word in a string
-sentence = "data science is fun and data science is powerful".split()
-for ch in sentence:
-   if sentence.count(ch) == 1:
-        print(ch)
+# #Second task of day 13 to find the unique word in a string
+# sentence = "data science is fun and data science is powerful".split()
+# for ch in sentence:
+#    if sentence.count(ch) == 1:
+#         print(ch)
     
     
+
+# my_info = "Hello my name is mohit kumar mishra "
+
+# file = open ("Basic.txt","w")
+# file.write(my_info)
+# file.close()
+
+
+# my_info = "Hello my name is mohit kumar mishra "
+
+students = [
+    "Amit Kumar, 20, Delhi, amit@example.com",
+    "Sneha Gupta, 21, Pune, sneha@example.com",
+    "Rohan Singh, 22, Bangalore, rohan@example.com",
+    "Anjali Mehta, 20, Chennai, anjali@example.com"
+]
+
+with open("students.txt", "w") as f:
+    for student in students:
+        f.write(student + "\n")
 
